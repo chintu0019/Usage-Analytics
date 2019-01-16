9fc98ee1a988a652586ed575c3668d418cf0e316
Manoj Kesavulu <manojpratnas@gmail.com>
2019-01-16 19:15:54 +0000

    update scripts to run experiments

--- Docker-Odoo-10.0/start-experiment.sh
+++ Docker-Odoo-10.0/start-experiment.sh
@@ -2,8 +2,14 @@
 
 echo "enter participant's name"
 read name
-mkdir ./odoo_logs/$name
-echo "Folder created ./odoo_logs $name"
-cd ./odoo_logs/$name
-echo "Logging to file: odoo_$name.log"
+if [ -d "$DIRECTORY" ]; then
+    mkdir ./odoo_logs/$name
+    echo "Folder created ./odoo_logs $name"
+    echo "Logging to file: odoo_$name.log"
+else 
+    echo "Folder with the name $name exists"
+    echo "Appending the logs to file: odoo_$name.log"
+fi
+
+systemctl start docker
 docker-compose up >> ./odoo_logs/$name/odoo_$name.log
