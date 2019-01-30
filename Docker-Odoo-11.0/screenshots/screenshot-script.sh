9c00cfa3159074dc6b14133001e6d9d0b4f16188
Paolo Bolzoni <paolo.bolzoni.brown@gmail.com>
2019-01-30 16:19:04 +0000

    Fix screenshot script when using names with spaces. Activate bash strict mode.

--- Docker-Odoo-11.0/screenshots/screenshot-script.sh
+++ Docker-Odoo-11.0/screenshots/screenshot-script.sh
@@ -1,20 +1,24 @@
-#! /bin/bash
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
 
 echo "enter participant's name"
 read name
 if [ -e "$name" ]
 then
     echo "Participant folder exist"
-    cd ./$name
+    cd ./"$name"
 else
     echo "Creating folder $name"
-    mkdir ./$name
-    cd ./$name
+    mkdir ./"$name"
+    cd ./"$name"
 fi
 
 
 echo "screenshots being stored in ./$name ...."
 while true;
 do
-    scrot -u -c '%y-%m-%d-%H-%M-%S.png'
+    scrot --focused '%y-%m-%d-%H-%M-%S.png'
 done
+
