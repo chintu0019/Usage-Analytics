ac09aaccb8cd92303a135454f94aeca2dfe653cd
Paolo Bolzoni <paolo.bolzoni.brown@gmail.com>
2019-02-27 18:10:18 +0000

    Add Odoo10 docker image with source code

---
+++ Odoo10/Odoo10/entrypoint.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -e
+
+if [ ! -f /init ] ;then
+    echo "Copying odoo code in the host"
+    rsync -rtu --delete /usr/lib/python2.7/dist-packages/odoo.orig/* "/usr/lib/python2.7/dist-packages/odoo"
+    touch /init
+fi
+
+# set the postgres database host, port, user and password according to the environment
+# and pass them as arguments to the odoo process if not present in the config file
+: ${HOST:=${DB_PORT_5432_TCP_ADDR:='db'}}
+: ${PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
+: ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
+: ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo'}}}
+
+DB_ARGS=()
+function check_config() {
+    param="$1"
+    value="$2"
+    if ! grep -q -E "^\s*\b${param}\b\s*=" "$ODOO_RC" ; then
+        DB_ARGS+=("--${param}")
+        DB_ARGS+=("${value}")
+   fi;
+}
+check_config "db_host" "$HOST"
+check_config "db_port" "$PORT"
+check_config "db_user" "$USER"
+check_config "db_password" "$PASSWORD"
+
+case "$1" in
+    -- | odoo)
+        shift
+        if [[ "$1" == "scaffold" ]] ; then
+            odoo "$@"
+        else
+            odoo "$@" "${DB_ARGS[@]}"
+        fi
+        ;;
+    -*)
+        exec odoo "$@" "${DB_ARGS[@]}"
+        ;;
+    *)
+        exec odoo "$@"
+esac
+
+exit 1
