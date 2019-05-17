83ce17e45010e094b1627380143c57b138aedcbf
Paolo Bolzoni <paolo.bolzoni.brown@gmail.com>
2019-05-17 12:56:40 +0100

    Add a start experiment bash scrit

---
+++ Odoo10/start_exp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+SCRIPTNAME="`readlink -e "$0"`"
+SCRIPTDIR="`dirname "$SCRIPTNAME"`"
+
+
+cd "$SCRIPTDIR"
+
+mkdir -p results
+docker-compose up
+
+cd odoo/csvfolder
+find  -type f  -iname '*.csv'  -size +63c  -exec echo cp '{}' ../../results ';'
+echo rm *
+
