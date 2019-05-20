227381479959eab613be1280f57be05ce4ee7c1d
Paolo Bolzoni <paolo.bolzoni.brown@gmail.com>
2019-05-20 12:15:52 +0100

    Fix a bug with logs shorter than 4 lines, improve readability

--- patterns.py
+++ patterns.py
@@ -10,7 +10,7 @@ def _longest_common_prefix(lhs, rhs):
     for i in range(0, min(len(lhs), len(rhs))):
         if lhs[i] != rhs[i]: break
     else:
-        i = i + 1
+        i += 1
     if isinstance(lhs, collections.Hashable):
         return lhs[:i]
     else:
@@ -19,10 +19,12 @@ def _longest_common_prefix(lhs, rhs):
 
 def _make_suffix_array(string):
     suffix_array = [i for i in range(0, len(string))]
-    def suffix_key(i):
-        return string[i:]
-    suffix_array.sort(key=suffix_key)
-    return suffix_array
+
+    def suffix_array_f(idx):
+        return string[idx:]
+    suffix_array.sort(key=suffix_array_f)
+
+    return suffix_array_f, len(suffix_array)
 
 
 def _starts_with(full, prefix):
@@ -35,26 +37,27 @@ def _starts_with(full, prefix):
     return True
 
 
+
 def find_repetitions(string, minl):
-    minl = _clamp(minl, 2, len(string) // 2)
-    suffix_array = _make_suffix_array(string)
+    minl = _clamp(minl, 2, max(2, len(string) // 2))
+    suffix_array, len_suffix_array = _make_suffix_array(string)
 
     patterns = {}
-    for w_idx in range(0, len(suffix_array) - 1):
-        word = _longest_common_prefix(string[suffix_array[w_idx]:], string[suffix_array[w_idx+1]:])
+    for w_idx in range(0, len_suffix_array - 1):
+        word = _longest_common_prefix(suffix_array(w_idx), suffix_array(w_idx+1))
         if len(word) < minl or word in patterns: continue
 
         patterns[word] = 1
 
         c_idx = w_idx - 1
-        while c_idx >= 0 and _starts_with(string[ suffix_array[c_idx]: ], word):
-            c_idx = c_idx - 1
-            patterns[word] = patterns[word] + 1
+        while c_idx >= 0 and _starts_with(suffix_array(c_idx), word):
+            c_idx -= 1
+            patterns[word] += 1
 
         c_idx = w_idx + 1
-        while c_idx < len(suffix_array) and _starts_with( string[ suffix_array[c_idx]: ], word):
-            c_idx = c_idx + 1
-            patterns[word] = patterns[word] + 1
+        while c_idx < len_suffix_array and _starts_with(suffix_array(c_idx), word):
+            c_idx += 1
+            patterns[word] += 1
 
     return patterns
 
