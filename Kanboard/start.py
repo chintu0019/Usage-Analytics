485bfb9db905ae80a3a934cfeddc7001d56ec3f9
Paolo Bolzoni <paolo.bolzoni.brown@gmail.com>
2019-07-05 17:13:54 +0100

    Add more actions

--- Kanboard/start.py
+++ Kanboard/start.py
@@ -7,17 +7,6 @@ import pprint
 from flask import request, Flask
 import csv_writer as cw
 
-cw.CsvWriter("./results", (
-    'timestamp',
-    'actionName',
-    'userId',
-    'ipAddr',
-    'id',
-    'stageId',
-    'partnerId',
-    'text',
-))
-
 
 def pp(v):
     pp.f(v)
@@ -30,7 +19,7 @@ def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 
-def just_print(json):
+def just_print(_csvwriter, json):
     eprint("Event", json['event_name'], "is uncovered")
     eprint("===============")
     pp(json)
@@ -48,10 +37,21 @@ def login():
     elif request.method == 'POST':
         json = request.get_json()
         f = login.dispatch.get(json['event_name'], just_print)
-        f(json)
-
+        f(login.csvwriter, json)
         return ""
+
 login.dispatch = {}
+login.csvwriter = cw.CsvWriter("./results", (
+    'timestamp',
+    'actionName',
+    'userId',
+    'ipAddr',
+    'id',
+    'stageId',
+    'partnerId',
+    'text',
+    'title',
+))
 
 
 def call_if(name):
@@ -60,10 +60,72 @@ def call_if(name):
         return f
     return call_if_impl
 
+# comment.update
+# comment.delete
+# file.create
+# task.move.project
+# task.move.position
+# task.move.swimlane
+# task.close
+# task.open
+# task.assignee_change
+# subtask.update
+# subtask.create
+# subtask.delete
+# task_internal_link.create_update
+# task_internal_link.delete
+
+
+@call_if('task.create')
+def task_create(csvw, json):
+    task = json['event_data']['task']
+    csvw.write({
+        'actionName': 'Create Note',
+        'userId': task['creator_id'],
+        'title': task['title'],
+        'text': task['description'],
+        'stageId': task['column_id'],
+        'id': json['event_data']['task_id'],
+    })
 
 
 
+@call_if('task.move.column')
+def task_move_column(csvw, json):
+    task = json['event_data']['task']
+    csvw.write({
+        'actionName': "Move Note",
+        'userId': task['creator_id'],
+        'title': task['title'],
+        'text': task['description'],
+        'stageId': json['event_data']['dst_column_id'],
+    })
 
+
+
+@call_if('task.update')
+def task_update(csvw, json):
+    task = json['event_data']['task']
+    csvw.write({
+        'actionName': "Edit Note",
+        'stageId': task['column_id'],
+        'userId': task['creator_id'],
+        'title': task['title'],
+        'text': task['description'],
+    })
+
+
+
+@call_if('comment.create')
+def comment_create(csvw, json):
+    comment = json['event_data']['comment']
+    csvw.write({
+        'actionName': "Comment in Note",
+        'userId': comment['user_id'],
+        'stageId': json['event_data']['task']['column_id'],
+        'id': comment['task_id'],
+        'text': comment['comment'],
+    })
 
 
 
