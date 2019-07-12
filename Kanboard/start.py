98389b045b82f8b02294dae97e8615212681f56d
Paolo Bolzoni <paolo.bolzoni.brown@gmail.com>
2019-07-12 17:06:20 +0100

    start.py flask server now logs all webhook actions

--- Kanboard/start.py
+++ Kanboard/start.py
@@ -46,11 +46,13 @@ login.csvwriter = cw.CsvWriter("./results", (
     'actionName',
     'userId',
     'ipAddr',
-    'id',
+    'id', # actually taskId
     'stageId',
     'partnerId',
     'text',
     'title',
+    'positionId',
+    'swimlaneId'
 ))
 
 
@@ -60,58 +62,44 @@ def call_if(name):
         return f
     return call_if_impl
 
-# comment.update
-# comment.delete
-# file.create
-# task.move.project
-# task.move.position
-# task.move.swimlane
-# task.close
-# task.open
-# task.assignee_change
-# subtask.update
-# subtask.create
-# subtask.delete
-# task_internal_link.create_update
-# task_internal_link.delete
+
+def get_task_data(json):
+    task = json['event_data']['task']
+    return {
+        'title': task['title'],
+        'text': task['description'],
+        'userId': task['creator_id'],
+        'stageId': task['column_id'],
+        'id': json['event_data']['task_id'],
+        'swimlaneId': task['swimlane_id'],
+    }
+
 
 
 @call_if('task.create')
 def task_create(csvw, json):
-    task = json['event_data']['task']
     csvw.write({
         'actionName': 'Create Note',
-        'userId': task['creator_id'],
-        'title': task['title'],
-        'text': task['description'],
-        'stageId': task['column_id'],
-        'id': json['event_data']['task_id'],
+        **get_task_data(json),
     })
 
 
 
 @call_if('task.move.column')
 def task_move_column(csvw, json):
-    task = json['event_data']['task']
     csvw.write({
-        'actionName': "Move Note",
-        'userId': task['creator_id'],
-        'title': task['title'],
-        'text': task['description'],
-        'stageId': json['event_data']['dst_column_id'],
+        'actionName': "Move Note Column",
+        'positionId': json['event_data']['position'],
+        **get_task_data(json),
     })
 
 
 
 @call_if('task.update')
 def task_update(csvw, json):
-    task = json['event_data']['task']
     csvw.write({
         'actionName': "Edit Note",
-        'stageId': task['column_id'],
-        'userId': task['creator_id'],
-        'title': task['title'],
-        'text': task['description'],
+        **get_task_data(json),
     })
 
 
@@ -127,6 +115,166 @@ def comment_create(csvw, json):
         'text': comment['comment'],
     })
 
+
+
+@call_if('comment.update')
+def comment_update(csvw, json):
+    comment = json['event_data']['comment']
+    csvw.write({
+        'actionName': "Comment Edit",
+        'userId': comment['user_id'],
+        'stageId': json['event_data']['task']['column_id'],
+        'id': comment['task_id'],
+        'text': comment['comment'],
+    })
+
+
+
+@call_if('comment.delete')
+def comment_delete(csvw, json):
+    comment = json['event_data']['comment']
+    csvw.write({
+        'actionName': "Comment Delete",
+        'userId': comment['user_id'],
+        'stageId': json['event_data']['task']['column_id'],
+        'id': comment['task_id'],
+        'text': comment['comment'],
+    })
+
+
+
+@call_if('task.file.create')
+def file_create(csvw, json):
+    file_data = json['event_data']['file']
+    csvw.write({
+        'actionName': "Attach File",
+        'userId': file_data['user_id'],
+        'stageId': json['event_data']['task']['column_id'],
+        'id': file_data['task_id'],
+        'text': file_data['name'],
+    })
+
+
+@call_if('task.move.position')
+def task_move_column(csvw, json):
+    csvw.write({
+        'actionName': "Move Note Position",
+        'positionId': json['event_data']['position'],
+        **get_task_data(json),
+    })
+
+
+@call_if('task.move.swimlane')
+def task_move_swimlane(csvw, json):
+    csvw.write({
+        'actionName': "Move Note Swimlane",
+        'positionId': json['event_data']['position'],
+        **get_task_data(json),
+    })
+
+
+@call_if('task.close')
+def task_close(csvw, json):
+    csvw.write({
+        'actionName': "Close Note",
+        **get_task_data(json),
+    })
+
+
+@call_if('task.open')
+def task_close(csvw, json):
+    csvw.write({
+        'actionName': "Open Note",
+        **get_task_data(json),
+    })
+
+
+@call_if('task.assignee_change')
+def task_assignee_change(csvw, json):
+    csvw.write({
+        'actionName': "Note Change Assignee",
+        **get_task_data(json),
+    })
+
+
+@call_if('subtask.create')
+def subtask_create(csvw, json):
+    task = json['event_data']['task']
+    csvw.write({
+        'title': task['title'],
+        'text': task['description'],
+        'userId': task['creator_id'],
+        'stageId': task['column_id'],
+        'swimlaneId': task['swimlane_id'],
+        'title': json['event_data']['subtask']['title'],
+        'positionId': json['event_data']['subtask']['position'],
+        'actionName': 'Subtask Create'
+    })
+
+
+@call_if('subtask.update')
+def subtask_update(csvw, json):
+    task = json['event_data']['task']
+    csvw.write({
+        'title': task['title'],
+        'text': task['description'],
+        'userId': task['creator_id'],
+        'stageId': task['column_id'],
+        'title': json['event_data']['subtask']['title'],
+        'positionId': json['event_data']['subtask']['position'],
+        'actionName': 'Subtask Update',
+    })
+
+
+@call_if('subtask.delete')
+def subtask_delete(csvw, json):
+    task = json['event_data']['task']
+    csvw.write({
+        'title': task['title'],
+        'text': task['description'],
+        'userId': task['creator_id'],
+        'stageId': task['column_id'],
+        'title': json['event_data']['subtask']['title'],
+        'positionId': json['event_data']['subtask']['position'],
+        'actionName': 'Subtask Delete',
+    })
+
+
+@call_if('task_internal_link.create_update')
+def task_internal_link_create_update(csvw, json):
+    task = json['event_data']['task']
+    csvw.write({
+        'actionName': 'Create Internal Link',
+        'partnerId': json['event_data']['task_link']['opposite_task_id'],
+        'title': task['title'],
+        'text': task['description'],
+        'userId': task['creator_id'],
+        'stageId': task['column_id'],
+        'id': json['event_data']['task_link']['id'],
+    })
+
+
+@call_if('task_internal_link.delete')
+def task_internal_link_delete(csvw, json):
+    task = json['event_data']['task']
+    csvw.write({
+        'actionName': 'Delete Internal Link',
+        'partnerId': json['event_data']['task_link']['opposite_task_id'],
+        'title': task['title'],
+        'text': task['description'],
+        'userId': task['creator_id'],
+        'stageId': task['column_id'],
+        'id': json['event_data']['task_link']['id'],
+    })
+
+
+
+@call_if('task.move.project')
+def task_move_project(csvw, json):
+    csvw.write({
+        'actionName': 'Move Note Project',
+        **get_task_data(json),
+    })
 
 
 if __name__ == '__main__':
