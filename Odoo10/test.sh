4b0617a34e6ba1fe7490ba2dcf9c70b43b0270a6
MK <manojpratnas@gmail.com>
2019-09-10 22:51:55 +0100

    python test script to play around with python commands

---
+++ Odoo10/test.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+SCRIPTNAME="`readlink -n -e "$0"`"
+SCRIPTDIR="`dirname "$SCRIPTNAME"`"
+
+#echo "SCRIPTNAME" $SCRIPTNAME
+#echo "SCRIPTDIR" $SCRIPTDIR
+
+#echo "arg" $1
+user=unnamed
+
+cd "$SCRIPTDIR"/results
+mkdir -p "$user"
+userdir="`readlink -n -e "$user"`"
+
+#echo "userdir" $userdir
+
+cd "$userdir"
+participants_file="`readlink -n -e "$SCRIPTDIR/results/Participants.csv"`"
+#echo "participants_file" $participants_file
+
+#unset IFS
+#while read -r userID username
+#    do
+#        echo "$userID $username"
+#    done < <(grep "" $participants_file)
+
+#userID=`tail -1 $participants_file | head -1 | cut -d',' -f1` 
+#echo "userID" $userID
+
+cd "$SCRIPTDIR"
+
+python update_userid.py $userdir $user
+
+#echo "clean up? [y/n]"
+#read response
+#if [ "$response" == 'y' ]
+#then 
+#    cd results
+#    rm -rf $user
+#fi
