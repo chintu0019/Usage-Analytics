725c1fd33e3d696ffa27b6431eeabc36f887bd01
MK <manojpratnas@gmail.com>
2021-02-01 08:41:38 +0000

    test analysis code file

--- Analytics-Code/test.py
+++ Analytics-Code/test.py
@@ -1,43 +1,29 @@
 #!/usr/bin/env python3
 
-import os
-import shutil
-from glob import glob
-from fnmatch import fnmatch
+from datetime import datetime
+import pandas
+from pathlib import Path
+
+from pandas.io.parsers import read_csv
 
 
-def listLogs(src):
-    pattern = "*.csv"
-    for path, subdirs, files in os.walk(src):
-        for name in files:
-            if fnmatch(name, pattern):
-                print(os.path.join(path, name))
-            #print("path = " + path + "\nsubdirs = " + subdirs + "\nfiles = ")
-            #print("path = " + "\nsubdirs = " + "\nfiles = ")
+participants_file = '../Scenario/Participants.csv'
+results_folder_10_path = '../Odoo10/results'
+results_folder_11_path = '../Odoo11/results'
+log_folder_10 = './Combined-log-folder/Log-folder-10'
+log_folder_11 = './Combined-log-folder/Log-folder-11'
+usage_data_file_10 = './Combined-log-folder/Usage-data-10.csv'
+usage_data_file_11 = './Combined-log-folder/Usage-data-11.csv'
+metrics_file_10 = "Insights/Metrics-10.csv"
+actions_file = "../Scenario/Actions_list.csv"
 
+def getTimestamp(date_time):
+    date_time_obj = datetime.strptime(date_time, '%Y-%m-%d %H:%M:%S.%f')
+    return datetime.timestamp(date_time_obj)
 
-def listLogs2(src):
-    pattern = "*.csv"
-    for path, subdirs, files in os.walk(src):
-        for name in files:
-            if fnmatch(name, pattern):
-                dirpath, dirname = os.path.split(path)
-                if name.startswith(dirname, 0, len(name)):
-                    print(os.path.join(path, name))
-
-
-def copyFiles():
-    src10  = '../Odoo10/results/'
-    src11  = os.listdir("../Odoo11/results/")
-    #dest10 = os.listdir("../Odoo10/Log-folder/")
-    #dest11 = os.listdir("../Odoo11/Log-folder/")
-
-    gsrc10  = glob("../Odoo10/results/")
-    gsrc11  = glob("../Odoo11/results/")
-    #gdest10 = glob("../Odoo10/Log-folder/")
-    #gdest11 = glob("../Odoo11/Log-folder/")
-
-    listLogs2(src10)
-
-
-copyFiles()    
+uFile = pandas.read_csv(usage_data_file_10)
+index = 1
+a = "actionName"
+print(uFile.at[index, "actionName"], uFile.at[index, "userId"], getTimestamp(uFile.at[index, "timestamp"]))
+print(uFile.at[index+1, "actionName"], uFile.at[index+1, "userId"], getTimestamp(uFile.at[index+1, "timestamp"]))
+print(getTimestamp(uFile.at[index+1, "timestamp"])-getTimestamp(uFile.at[index, "timestamp"]))
