7ff8a16942cc6ef56562d0e7729380323581a8c2
MK <manojpratnas@gmail.com>
2021-02-01 08:41:24 +0000

    frequency analysis file

--- Analytics-Code/frequency.py
+++ Analytics-Code/frequency.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 
-import csv
-import json
+
+import pandas
 
 
-def getCSV(odoo_version):
-    if odoo_version == '10':
-        #TODO - update code
+def updateFrequencyActionName(odoo_version, ud_userId, ud_actionName):
+    mFile = pandas.read_csv(metrics_file)
+    for index, row in mFile.iterrows():
+        if row["userId"] == ud_userId and row["actionName"] == ud_actionName:
+            mFile.at[index, "frequency"] += 1
+    mFile.to_csv(metrics_file, index=False)
 
 
-def start(odoo_version):
-    getCSV(odoo_version)
+def start(odoo_version, usage_data_file):
+    global metrics_file 
+    metrics_file = "Insights/Metrics-"+odoo_version+".csv"
+    usage_data = pandas.read_csv(usage_data_file)
+    for index, row in usage_data.iterrows():
+        updateFrequencyActionName(odoo_version, row["userId"], row["actionName"])
