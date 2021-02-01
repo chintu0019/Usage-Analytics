ad00d7cad98ee1f8a4aed89dc37afcd6fd0fefd0
MK <manojpratnas@gmail.com>
2021-02-01 18:42:31 +0000

    timespent analysis file V1

--- Analytics-Code/timespent.py
+++ Analytics-Code/timespent.py
@@ -3,8 +3,6 @@
 
 import pandas
 from datetime import datetime
-from numpy.core.numeric import NaN
-from fnmatch import fnmatch
 
 
 def toTimestamp(date_time):
@@ -24,9 +22,8 @@ def updateTimespentAction(usage_data_file, ud_userId, ud_actionName):
     time_spent = 0
 
     for index, row in uDF.iterrows():
-        if index == len(uDF): break
-        next_index = index+1
-        if row["userId"] == ud_userId and row["actionName"] == ud_actionName and row["userId"] == uDF.at[next_index, "userId"]: #BUG - error here with index+1 or next_index
+        if index == len(uDF)-1: break # break the loop if this is the last item in the loop
+        if row["actionName"] == ud_actionName:
             time_spent = time_spent + (getTimestamp(index+1, uDF) - getTimestamp(index, uDF))
 
     for index, row in mDF.iterrows():
@@ -35,9 +32,10 @@ def updateTimespentAction(usage_data_file, ud_userId, ud_actionName):
     mDF.to_csv(metrics_file, index=False)
     
 
-def start(odoo_version, usage_data_file):
+def analyse(odoo_version, log_files_list):
     global metrics_file
-    metrics_file = "Insights/Metrics-"+odoo_version+".csv"
-    usage_data = pandas.read_csv(usage_data_file)
-    for index, row in usage_data.iterrows():
-        updateTimespentAction(usage_data_file, row['userId'], row['actionName'])
+    metrics_file = "Insights/Metrics-" + odoo_version + ".csv"
+    for file in log_files_list:
+        usage_data = pandas.read_csv(file)
+        for index, row in usage_data.iterrows():
+            updateTimespentAction(file, row["userId"], row["actionName"])
