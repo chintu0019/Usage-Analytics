d9a969fc6be2ac5f3a08b8a798138b4c6c558e8e
MK <manojpratnas@gmail.com>
2021-02-05 11:22:18 +0000

    start-analysis.py update - remove code to handle combined logs

--- Analytics-Code/start_analysis.py
+++ Analytics-Code/start_analysis.py
@@ -17,14 +17,7 @@ from fnmatch import fnmatch
 
 
 participants_file = Path("../Scenario/Participants.csv")
-results_folder_10_path = '../Odoo10/results'
-results_folder_11_path = '../Odoo11/results'
-log_folder_10 = './Combined-log-folder/Log-folder-10'
-log_folder_11 = './Combined-log-folder/Log-folder-11'
-usage_data_file_10 = './Combined-log-folder/Usage-data-10.csv'
-usage_data_file_11 = './Combined-log-folder/Usage-data-11.csv'
-metrics_file_10 = "Insights/Metrics-10.csv"
-metrics_file_11 = "Insights/Metrics-11.csv"
+metrics_consistency_file = "Insights/Metrics-consistency.csv"
 actions_file = "../Scenario/Actions_list.csv"
 
 
@@ -66,61 +59,78 @@ def getFileList(path):
     return filesToFind
 
 
-def updateCombinedLogFile(odoo_version):
-    participants_list = getParticipantsNames()
-    #print(participants_list)
-    fileList = []
-
-    if odoo_version == '10':
-        fileList = getFileList(results_folder_10_path)
-        copyFiles(fileList, log_folder_10)
-        #combine all files in the list
-        combined_csv = pandas.concat([pandas.read_csv(f) for f in fileList])
-        #export to csv
-        combined_csv.to_csv(usage_data_file_10, index=False, encoding='utf-8-sig')
-        usage_data_file = Path(usage_data_file_10)
-
-    elif odoo_version == '11':
-        fileList = getFileList(results_folder_11_path)
-        copyFiles(fileList, log_folder_11)
-        combined_csv = pandas.concat([pandas.read_csv(f) for f in fileList])
-        combined_csv.to_csv(usage_data_file_11, index=False, encoding='utf-8-sig')
-        usage_data_file = Path(usage_data_file_11)
-    return usage_data_file
+#def updateCombinedLogFile(odoo_version):
+#    participants_list = getParticipantsNames()
+#    #print(participants_list)
+#    fileList = []
+#
+#    if odoo_version == '10':
+#        fileList = getFileList(results_folder_10_path)
+#        copyFiles(fileList, log_folder_10)
+#        #combine all files in the list
+#        combined_csv = pandas.concat([pandas.read_csv(f) for f in fileList])
+#        #export to csv
+#        combined_csv.to_csv(usage_data_file_10, index=False, encoding='utf-8-sig')
+#        usage_data_file = Path(usage_data_file_10)
+#
+#    elif odoo_version == '11':
+#        fileList = getFileList(results_folder_11_path)
+#        copyFiles(fileList, log_folder_11)
+#        combined_csv = pandas.concat([pandas.read_csv(f) for f in fileList])
+#        combined_csv.to_csv(usage_data_file_11, index=False, encoding='utf-8-sig')
+#        usage_data_file = Path(usage_data_file_11)
+#    return usage_data_file
 
 
 def initialiseMetricsFile(odoo_version):
+    print("Odoo "+odoo_version+": Initialize metrics files...")
     actions_data = pandas.read_csv(actions_file)
     actionsList = actions_data.actionName.to_list()
     userIdList = getParticipantsIds()
 
-    with open(metrics_file_10, mode='w') as mFile:
-        csv_header = ['userId', 'actionName', 'frequency', 'timeSpent', 'consistency']
+    metrics_ft_file = "Insights/Metrics-"+odoo_version+"-frequency-timespent.csv"
+
+    with open(metrics_ft_file, mode='w') as mFile:
+        print("Odoo "+odoo_version+": Initialise", metrics_ft_file)
+        csv_header = ['userId', 'actionName', 'occurrences', 'frequency', 'timespent']
         csv_writer = csv.writer(mFile)
         csv_writer.writerow(csv_header)
         for userId in userIdList:
             for action in actionsList:
-                csv_writer.writerow([userId, action, 0, 0, 0])
+                csv_writer.writerow([userId, action, 0.0, 0.0, 0.0])
+    
+    with open(metrics_consistency_file, mode='w') as mFile:
+        print("Odoo "+odoo_version+": Initialise", metrics_consistency_file)
+        csv_header = ['userId', 'actionName', 'consistency (frequency)', 'consistency (timespent)']
+        csv_writer = csv.writer(mFile)
+        csv_writer.writerow(csv_header)
+        for userId in userIdList:
+            for action in actionsList:
+                csv_writer.writerow([userId, action, 0.0])
+    print("\nInitialisation Complete!")
 
 
 def realMain(odoo_version):
-    usage_data_file = updateCombinedLogFile(odoo_version)
+    #usage_data_file = updateCombinedLogFile(odoo_version)
+    results_folder_path = "../Odoo" + odoo_version + "/results"
+    log_files_list = getFileList(results_folder_path)
     initialiseMetricsFile(odoo_version)
-    frequency.start(odoo_version, usage_data_file)
-    timespent.start(odoo_version, usage_data_file)
+    timespent.analyse(odoo_version, log_files_list)
+    frequency.analyse(odoo_version, log_files_list)
     
-    #TODO - call frequency, timespent and consistency metrics
-    #TODO - behavioural score
-
 
 def main(argv=None):
     prgname = os.path.basename(__file__) if '__file__' in globals() else 'prg'
     if argv is None:
         argv = sys.argv
 
-    odoo_version = argv[1]
-    #print(odoo_version)
-    realMain(odoo_version)
+    #odoo_version = argv[1]
+    print("\n----- Odoo Version 10 -----")
+    realMain("10")
+    print("\n\n----- Odoo Version 11 -----")
+    realMain("11")
+    consistency.analyse()
+    
     #try:
     #    odoo_version = argv[1]
     #    #print(odoo_version)
